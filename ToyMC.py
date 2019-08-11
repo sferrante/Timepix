50f5603529ea27a97be7e7c3cef6d2e45009c925
sferrante <sef115@scarletmail.rutgers.edu>
2019-08-10 20:40:52 -0400

    Create ToyMC.py

---
+++ ToyMC.py
@@ -0,0 +1,12 @@
+import numpy as np
+%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import glob
+import time
+import random
+from scipy.stats import norm
+plt.rcParams["figure.figsize"] = (3,3)
+
+print(time.ctime())
